eaff63f3b3ade2d3b2f31ee285a017fea03cf582
Teamunicorn246 <ebba.cederlof@elev.ga.ntig.se>
2022-09-07 14:11:25 +0200

    Add project files.

---
+++ src/SchoolSystem.cpp
@@ -0,0 +1,27 @@
+#include "SchoolSystem.h"
+
+void SchoolSystem::Run()
+{
+	while (true)
+	{
+		//PrintMenu();
+		//if (input == 1) AddStudent();
+		//if (input == 1) AddStudent();
+		//if (input == 1) AddStudent();
+		//if (input == 1) AddStudent();
+		//if (input == 1) AddStudent();
+		//if (input == 1) AddStudent();
+	}
+}
+
+void SchoolSystem::AddStudent()
+{
+	Student student;
+	student.name = "Blabla";
+	student.age = 123;
+	students.push_back(student);
+}
+
+void SchoolSystem::RemoveStudent()
+{
+}
